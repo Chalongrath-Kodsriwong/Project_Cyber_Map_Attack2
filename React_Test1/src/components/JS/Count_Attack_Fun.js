07740370697c70c6ce72bdb2066db66d75618f51
bonuszaa125 <bonuszaa125@gmail.com>
2024-12-15 14:48:13 +0700

    Update_ArrowOnAttack_Summary

--- React_Test1/src/components/JS/Count_Attack_Fun.js
+++ React_Test1/src/components/JS/Count_Attack_Fun.js
@@ -4,8 +4,9 @@ import $ from 'jquery';
 let isHidden = true; // Tracks visibility of container-item
 let isAnimating = false; // Prevents repeated animations during a single click
 
-const Arrow = document.querySelector(".arrow");
-const btnShowHide = document.querySelector(".btn-showhide");
+const btnShowHide = $(".btn-showhide");
+const Arrow = $(".arrow");
+
 let status = true;
 
 export const setupCountAttackAnimation = () => {
@@ -25,6 +26,20 @@ export const setupCountAttackAnimation = () => {
           isAnimating = false; // Allow new animation after completion
         }
       );
+      $(".btn-showhide").mouseenter(function () { 
+        $(".arrow").css({
+          display: "inline",
+          transform: "rotate(-90deg)",
+          opacity: "1",
+          color: "#00bcd4"
+        })
+      });
+      $(".btn-showhide").mouseleave(function () { 
+        $(".arrow").css({
+          display: "none",
+          opacity: "0"
+        })
+      });
     } else {
       // Show container-item and move Classification up
       $(".toleftsize").animate(
@@ -37,9 +52,24 @@ export const setupCountAttackAnimation = () => {
           isAnimating = false; // Allow new animation after completion
         }
       );
+      $(".btn-showhide").mouseenter(function () { 
+        $(".arrow").css({
+          display: "inline",
+          transform: "rotate(90deg)",
+          opacity: "1",
+          color: "#00bcd4"
+        })
+      });
+      $(".btn-showhide").mouseleave(function () { 
+        $(".arrow").css({
+          display: "none",
+          opacity: "0"
+        })
+      });
     }
 
     isHidden = !isHidden; // Toggle visibility state
   });
+  
   
 };
